2e9058a6caa3cb8d29d47242e2072f03af424fff
Savan Patel <savanpatel39@gmail.com>
2018-04-13 12:28:19 -0400

    Update 2

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -5,12 +5,29 @@ const {MongoClient, ObjectID} = require('mongodb');
 // var obj = new ObjectID();
 // console.log(obj);
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) => {
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, client) => {
   if(err)
   {
     return console.log("Unable to connect mongodb server....");
   }
   console.log("Connect to mongodb server...");
+const db = client.db('TodoApp');
+
+//Delete many data at once
+// db.collection('Todos').deleteMany({text: 'Something to do'}).then((result) => { 
+//   console.log(result);
+// });
+
+//Delete only first data that is found
+// db.collection('Todos').deleteOne({text:'Something to do 1' }).then((result) => {
+//   console.log(result);
+// });
+
+//finds one and deletes it
+
+db.collection('Todos').findOneAndDelete({text: 'Something to do 2'}).then((result)=>{
+console.log(result);
+});
 
 //   db.collection('Todos').find({
 //       _id:new ObjectID('5a1bbcacf6b5600ebc505ec4')
