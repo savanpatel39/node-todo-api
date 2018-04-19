7f1cef7c37569d6fefe0303a6efe714e6f95728f
Savan Patel <savanpatel39@gmail.com>
2018-04-18 20:30:08 -0400

    Add patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -50,7 +51,7 @@ app.get('/todos/:id',(req,res)=>{
     });
 
 
-})
+});
 
 app.delete('/todos/:id', (req,res) => {
     var id = req.params.id;
@@ -66,7 +67,32 @@ app.delete('/todos/:id', (req,res) => {
     }).catch((err) => {
         res.status(404).send();
     });
-})
+});
+
+app.patch('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text','completed']);
+
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt  = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set:body},{new:true}).then((todo)=> {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    });
+});
 
 app.listen(port, () => {
     console.log("Started up port : ",port);
